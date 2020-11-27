196623e315163f6f70459c28bf394bfc90d030a4
David <dvdlee41@gmail.com>
2020-11-27 23:55:56 +0800

    Refactoring code by creating createNavController method

---
+++ AppStoreJSONAPIs/Controller/BaseTabBarViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BaseTabBarViewController.swift
+//  AppStoreJSONAPIs
+//
+//  Created by David on 2020/11/27.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarViewController: UITabBarController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+            
+    viewControllers = [
+      createNavController(viewController: UIViewController(), title: "Today", imageName: "today_icon"),
+      createNavController(viewController: UIViewController(), title: "Apps", imageName: "apps"),
+      createNavController(viewController: UIViewController(), title: "Search", imageName: "search")
+    ]
+  }
+  
+  fileprivate func createNavController(
+    viewController: UIViewController,
+    title: String,
+    imageName: String) -> UIViewController {
+    
+    let navController = UINavigationController(rootViewController: viewController)
+    navController.navigationBar.prefersLargeTitles = true
+    viewController.navigationItem.title = title
+    viewController.view.backgroundColor = .white
+    navController.tabBarItem.title = title
+    navController.tabBarItem.image = UIImage(named: imageName)
+    
+    return navController
+  }
+}
