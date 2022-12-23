2e644a859d62d5b474c7135d6239eebbffff21f0
David Lee <david@Davids-MacBook-Pro.local>
2022-12-24 02:02:50 +0800

    refactor: closeButton

--- AppStoreJSONAPIs/Controllers/Today/AppFullScreenController.swift
+++ AppStoreJSONAPIs/Controllers/Today/AppFullScreenController.swift
@@ -8,21 +8,28 @@
 
 import UIKit
 
-class AppFullScreenController: UITableViewController {
+class AppFullScreenController: UIViewController, UITableViewDataSource, UITableViewDelegate {
   // MARK: - Instance Properties
+  let tableView = UITableView(frame: .zero, style: .plain)
   var dismissHandler: (() ->())?
-  var todayItem: TodayItem?    
+  var todayItem: TodayItem?
   
-  override func scrollViewDidScroll(_ scrollView: UIScrollView) {
-    if scrollView.contentOffset.y < 0 {
-      scrollView.isScrollEnabled = false
-      scrollView.isScrollEnabled = true
-    }
-  }
+  let closeButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.setImage(#imageLiteral(resourceName: "close_button"), for: .normal)
+    return button
+  }()
   
   // MARK: - View Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    view.addSubview(tableView)
+    tableView.fillSuperview()
+    tableView.dataSource = self
+    tableView.delegate = self
+    
+    setupCloseButton()
+    
     tableView.tableFooterView = UIView()
     tableView.separatorStyle = .none
     tableView.allowsSelection = false
@@ -32,11 +39,11 @@ class AppFullScreenController: UITableViewController {
   }
   
   // MARK: - UITableViewDataSource Methods
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return 2
   }
   
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
     if indexPath.item == 0 {
       let headerCell = AppFullScreenHeaderCell()
@@ -52,16 +59,50 @@ class AppFullScreenController: UITableViewController {
   }
   
   // MARK: - UITableViewDelegate Methods
-  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     if indexPath.row == 0 {
       return TodayController.cellSize
     }
-    return super.tableView(tableView, heightForRowAt: indexPath)
+    return UITableView.automaticDimension
+  }
+  
+  // MARK: - UIScroollViewDelegate Methods
+  func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    if scrollView.contentOffset.y < 0 {
+      scrollView.isScrollEnabled = false
+      scrollView.isScrollEnabled = true
+    }
   }
   
   // MARK: - Selector Methods
   @objc fileprivate func handleDismiss(button: UIButton) {
     button.isHidden = true
     dismissHandler?()
+  }
+  
+  // MARK: - Helper Methods
+  fileprivate func setupCloseButton() {
+    view.addSubview(closeButton)
+    closeButton.anchor(
+      top: view.safeAreaLayoutGuide.topAnchor,
+      leading: nil,
+      bottom: nil,
+      trailing: view.trailingAnchor,
+      padding: .init(
+        top: 12,
+        left: 0,
+        bottom: 0,
+        right: 0
+      ),
+      size: .init(
+        width: 80,
+        height: 40
+      )
+    )
+    closeButton.addTarget(
+      self,
+      action: #selector(handleDismiss),
+      for: .touchUpInside
+    )
   }
 }
